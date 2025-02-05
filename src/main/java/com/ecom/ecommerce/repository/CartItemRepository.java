4d6a236ebc736426e0db02e5a76be9c1f237e558
Prakhar Bhatnagar <92019275+prakharbhatnagar20@users.noreply.github.com>
2025-02-05 16:58:47 +0530

    cart service

--- src/main/java/com/ecom/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/ecom/ecommerce/repository/CartItemRepository.java
@@ -1,16 +1,16 @@
 package com.ecom.ecommerce.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
-// import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-// import com.ecom.ecommerce.model.Cart;
-// import com.ecom.ecommerce.model.CartItem;
-// import com.ecom.ecommerce.model.Product;
+import com.ecom.ecommerce.model.Cart;
+import com.ecom.ecommerce.model.CartItem;
+import com.ecom.ecommerce.model.Product;
 
-// public interface CartItemRepository extends JpaRepository<CartItem, Long>{
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{
 
-//     @Query("SELECT ci FROM CartItem ci WHERE ci.cart=:cart AND ci.product=:product AND ci.size=:size AND ci.user.id=:userId")
-//     public CartItem isCartItemExist(@Param("cart") Cart cart, @Param("product") Product product, @Param("size") String size, @Param("userId") Long userId);
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart=:cart AND ci.product=:product AND ci.size=:size AND ci.userId=:userId")
+    public CartItem isCartItemExist(@Param("cart") Cart cart, @Param("product") Product product, @Param("size") String size, @Param("userId") Long userId);
     
-// }
+}
--- src/main/java/com/ecom/ecommerce/service/CartService.java
+++ src/main/java/com/ecom/ecommerce/service/CartService.java
@@ -1,17 +1,17 @@
 package com.ecom.ecommerce.service;
 
-// import com.ecom.ecommerce.exception.ProductException;
-// import com.ecom.ecommerce.exception.UserException;
-// import com.ecom.ecommerce.model.Cart;
-// import com.ecom.ecommerce.model.User;
-// import com.ecom.ecommerce.request.AddItemRequest;
+import com.ecom.ecommerce.exception.ProductException;
+import com.ecom.ecommerce.exception.UserException;
+import com.ecom.ecommerce.model.Cart;
+import com.ecom.ecommerce.model.User;
+import com.ecom.ecommerce.request.AddItemRequest;
 
-// public interface CartService {
+public interface CartService {
 
-//     public Cart createCart(User user);
+    public Cart createCart(User user);
 
-//     public String addCartItem(Long userId, AddItemRequest addItemRequest) throws UserException, ProductException;
+    public String addCartItem(Long userId, AddItemRequest addItemRequest) throws UserException, ProductException;
 
-//     public Cart findUserCart(Long userId) throws UserException;
+    public Cart findUserCart(Long userId) throws UserException;
     
-// }
+}

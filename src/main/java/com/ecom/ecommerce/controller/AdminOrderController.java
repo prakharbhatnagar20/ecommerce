1b5cfe53efa59a93390a6a4120ed7e7dfb221634
Prakhar Bhatnagar <92019275+prakharbhatnagar20@users.noreply.github.com>
2025-02-11 21:51:43 +0530

    order Service

--- src/main/java/com/ecom/ecommerce/controller/AdminOrderController.java
+++ src/main/java/com/ecom/ecommerce/controller/AdminOrderController.java
@@ -1,60 +1,60 @@
 package com.ecom.ecommerce.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestHeader;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.ecom.ecommerce.exception.OrderException;
-// import com.ecom.ecommerce.model.Order;
-// import com.ecom.ecommerce.response.ApiResponse;
-// import com.ecom.ecommerce.service.OrderService;
+import com.ecom.ecommerce.exception.OrderException;
+import com.ecom.ecommerce.model.Order;
+import com.ecom.ecommerce.response.ApiResponse;
+import com.ecom.ecommerce.service.OrderService;
 
-// @RestController
-// @RequestMapping("/api/admin/order")
-// public class AdminOrderController {
+@RestController
+@RequestMapping("/api/admin/order")
+public class AdminOrderController {
 
-//     @Autowired
-//     private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
 
-//     @GetMapping("/")
-//     public ResponseEntity<List<Order>> getAllOdersHandler() {
-//         List<Order> orders = orderService.getAllOrders();
-//         return new ResponseEntity<List<Order>>(orders, HttpStatus.OK);
-//     }
+    @GetMapping("/")
+    public ResponseEntity<List<Order>> getAllOdersHandler() {
+        List<Order> orders = orderService.getAllOrders();
+        return new ResponseEntity<List<Order>>(orders, HttpStatus.OK);
+    }
 
-//     @PutMapping("/{orderId}/confirmed")
-//     public ResponseEntity<Order> ConfirmOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
-//         Order order = orderService.confirmedOrder(orderId);
-//         return new ResponseEntity<Order>(order, HttpStatus.OK);
+    @PutMapping("/{orderId}/confirmed")
+    public ResponseEntity<Order> ConfirmOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
+        Order order = orderService.confirmedOrder(orderId);
+        return new ResponseEntity<Order>(order, HttpStatus.OK);
         
-//     }
+    }
     
-//     @PutMapping("/{orderId}/shipped")   
-//     public ResponseEntity<Order> shippedOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
-//         Order order = orderService.shippedOrder(orderId);
-//         return new ResponseEntity<Order>(order, HttpStatus.OK);
-//     }
-//     @PutMapping("/{orderId}/delivered")
-//     public ResponseEntity<Order> deliveredOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
-//         Order order = orderService.deliveredOrder(orderId);
-//         return new ResponseEntity<Order>(order, HttpStatus.OK);
-//     }
+    @PutMapping("/{orderId}/shipped")   
+    public ResponseEntity<Order> shippedOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
+        Order order = orderService.shippedOrder(orderId);
+        return new ResponseEntity<Order>(order, HttpStatus.OK);
+    }
+    @PutMapping("/{orderId}/delivered")
+    public ResponseEntity<Order> deliveredOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
+        Order order = orderService.deliveredOrder(orderId);
+        return new ResponseEntity<Order>(order, HttpStatus.OK);
+    }
 
-//     @DeleteMapping("/{orderId}/delete")
-//     public ResponseEntity<Order> deleteOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
-//         orderService.deleteOrder(orderId);
-//         ApiResponse res=new ApiResponse();
-//         res.setMessage("order deleted successfully");
-//         res.setStatus(true);
-//         return new ResponseEntity<Order>(HttpStatus.OK);
-//     }
-// }
+    @DeleteMapping("/{orderId}/delete")
+    public ResponseEntity<Order> deleteOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") String token) throws OrderException{
+        orderService.deleteOrder(orderId);
+        ApiResponse res=new ApiResponse();
+        res.setMessage("order deleted successfully");
+        res.setStatus(true);
+        return new ResponseEntity<Order>(HttpStatus.OK);
+    }
+}
--- src/main/java/com/ecom/ecommerce/controller/OrderController.java
+++ src/main/java/com/ecom/ecommerce/controller/OrderController.java
@@ -1,56 +1,56 @@
 package com.ecom.ecommerce.controller;
 
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestHeader;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.ecom.ecommerce.exception.OrderException;
-// import com.ecom.ecommerce.exception.UserException;
-// import com.ecom.ecommerce.model.Address;
-// import com.ecom.ecommerce.model.Order;
-// import com.ecom.ecommerce.model.User;
-// import com.ecom.ecommerce.service.OrderService;
-// import com.ecom.ecommerce.service.UserService;
+import com.ecom.ecommerce.exception.OrderException;
+import com.ecom.ecommerce.exception.UserException;
+import com.ecom.ecommerce.model.Address;
+import com.ecom.ecommerce.model.Order;
+import com.ecom.ecommerce.model.User;
+import com.ecom.ecommerce.service.OrderService;
+import com.ecom.ecommerce.service.UserService;
 
-// @RestController
-// @RequestMapping("/api/orders")
-// public class OrderController {
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
 
-//     @Autowired
-//     private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
 
-//     @Autowired
-//     private UserService userService;
+    @Autowired
+    private UserService userService;
 
-//     @GetMapping("/")
-//     public ResponseEntity<Order> createOrder(@RequestBody Address address, @RequestHeader("Authorization") String token) throws UserException {
+    @GetMapping("/")
+    public ResponseEntity<Order> createOrder(@RequestBody Address address, @RequestHeader("Authorization") String token) throws UserException {
        
-//         User user = userService.findUserByJwt(token);
-//         Order order = orderService.createOrder(user, address);
+        User user = userService.findUserByJwt(token);
+        Order order = orderService.createOrder(user, address);
 
-//         return new ResponseEntity<Order>(order, HttpStatus.CREATED);
-//     }
+        return new ResponseEntity<Order>(order, HttpStatus.CREATED);
+    }
 
-//     @GetMapping("/user")
-//     public ResponseEntity<List<Order>> userOrderHistory(@RequestHeader("Authorization") String token) throws UserException {
-//         User user = userService.findUserByJwt(token);
-//         List<Order> orders = orderService.userOrderHistory(user.getId());
-//         return new ResponseEntity<List<Order>>(orders, HttpStatus.OK);
-//     }
+    @GetMapping("/user")
+    public ResponseEntity<List<Order>> userOrderHistory(@RequestHeader("Authorization") String token) throws UserException {
+        User user = userService.findUserByJwt(token);
+        List<Order> orders = orderService.userOrderHistory(user.getId());
+        return new ResponseEntity<List<Order>>(orders, HttpStatus.OK);
+    }
 
-//     @GetMapping("/{orderId}")
-//     public ResponseEntity<Order> getOrderById(@RequestHeader("Authorization") String token, Long orderId) throws UserException, OrderException {
-//         User user = userService.findUserByJwt(token);
-//         Order order = orderService.findOrderById(orderId);
-//         return new ResponseEntity<Order>(order, HttpStatus.OK);
-//     }
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@RequestHeader("Authorization") String token, Long orderId) throws UserException, OrderException {
+        User user = userService.findUserByJwt(token);
+        Order order = orderService.findOrderById(orderId);
+        return new ResponseEntity<Order>(order, HttpStatus.OK);
+    }
     
-// }
+}

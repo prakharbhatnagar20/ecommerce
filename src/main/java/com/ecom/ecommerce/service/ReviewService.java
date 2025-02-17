8364119fda001bd682cda4998b54c7c81eecd404
Prakhar Bhatnagar <92019275+prakharbhatnagar20@users.noreply.github.com>
2025-02-18 00:29:15 +0530

    rating and review api added

--- src/main/java/com/ecom/ecommerce/service/ReviewService.java
+++ src/main/java/com/ecom/ecommerce/service/ReviewService.java
@@ -1,14 +1,14 @@
 package com.ecom.ecommerce.service;
 
-// import java.util.List;
+import java.util.List;
 
-// import com.ecom.ecommerce.exception.ProductException;
-// import com.ecom.ecommerce.model.Review;
-// import com.ecom.ecommerce.model.User;
-// import com.ecom.ecommerce.request.ReviewRequest;
+import com.ecom.ecommerce.exception.ProductException;
+import com.ecom.ecommerce.model.Review;
+import com.ecom.ecommerce.model.User;
+import com.ecom.ecommerce.request.ReviewRequest;
 
-// public interface ReviewService {
+public interface ReviewService {
     
-//     public Review createReview(ReviewRequest req, User user) throws ProductException;
-//     public List<Review> getAllReview(Long productId);
-// }
+    public Review createReview(ReviewRequest req, User user) throws ProductException;
+    public List<Review> getAllReview(Long productId);
+}
